14cc88472d42995838e43e60dbe2f5de1a944f08
masa69 <psychedelic.nekopunch@gmail.com>
2015-11-15 09:20:40 +0900

    update

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,10 @@ var sass   = require('gulp-sass');
 gulp.task('default', function()
 {
 	gulp.src('sass/style.scss')
-		.pipe(sass())
+		.pipe(sass({
+			// includePaths : [require("bourbon").includePaths],
+			// [outputStyle] Type: String Default: nested Values: nested, expanded, compact, compressed
+			// outputStyle  : 'compressed',
+		}))
 		.pipe(gulp.dest('./css/'));
 });
